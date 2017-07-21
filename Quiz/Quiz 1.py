a439ad3bfb05852178bdb1c02c7d0be3a4697571
Paul Hartmann <paul.hartmann@PAEDML-LINUX.LOKAL>
2017-07-21 11:27:22 +0200

    Lösung

--- Quiz/Quiz 1.py
+++ Quiz/Quiz 1.py
@@ -2,14 +2,14 @@ while True:
 
     print("Willkommen zur Quizshow ...\n !go or stand!")
     print("Nun auch gleich zu meiner ersten Frage")
-    print("Wer schläft länger? \na) der Elefant b) die Giraffe\n c)das Faultier oder d) die Fledermaus")
-    antwort = input("a), b), c), d)(schreiben sie es ohne Klammer)")
-    if antwort == "d" or antwort == "d)":
+    print("Wer schläft länger? \na) die Fledermaus b) die Giraffe\n c)das Faultier oder d) Der Elefant")
+    antwort = input("a), b), c), d)")
+    if antwort == "a" or antwort == "a)":
         print("Glückwunsch das war richtig")
         print("Nun zur nächsten frage")
         print("Welchem afrikanischen Raubtier\n fallen am meisten Menschen zum Opfer?")
-        antwort2 = input("a)Löwe, b)Jaguar,\n c)Krokodil d)Gepard")
-    if antwort2 == "c" or antwort2 == "c)":
+        antwort2 = input("a)Krokodil b)Jaguar,\n c)Löwe d)Gepard")
+    if antwort2 == "a" or antwort2 == "a)":
         print("Herzlichen Glückwunsch auch dass war richtig")
         print("weiter")
         print("Wie lang ist ein Tag auf dem Jupiter?")
@@ -21,13 +21,7 @@ while True:
         print("Haha du Opfer")
 
         
-    else:
-        print("Haha du Opfer")
-
-
-    else:
-        print("HAHA du Opfer")
-                
+                 
     antwort = input("\nWollen sie es noch einmal versuchen? (yes/no) ")
 
     if antwort != "yes":
