bd496c3370fe5dba8e9a1203d6523cf091fb1489
Paul Hartmann <paul.hartmann@PAEDML-LINUX.LOKAL>
2017-07-21 11:16:46 +0200

    Lösung

--- Quiz/Quiz 1.py
+++ Quiz/Quiz 1.py
@@ -9,29 +9,28 @@ while True:
         print("Nun zur nächsten frage")
         print("Welchem afrikanischen Raubtier\n fallen am meisten Menschen zum Opfer?")
         antwort2 = input("a)Löwe, b)Jaguar,\n c)Krokodil d)Gepard")
-        if antwort2 == "c" or antwort2 == "c)":
-            print("Herzlichen Glückwunsch auch dass war richtig")
-            print("weiter")
-            print("Wie lang ist ein Tag auf dem Jupiter?")
-            antwort3 = input("a) 9.50h b)24.00h c)12.20 d)22.40")
-            if antwort3 == "a" or antwort3 == "a)":
-                print("Sie haben es GESCHAFT ,fertig")
+    if antwort2 == "c" or antwort2 == "c)":
+        print("Herzlichen Glückwunsch auch dass war richtig")
+        print("weiter")
+        print("Wie lang ist ein Tag auf dem Jupiter?")
+        antwort3 = input("a) 9.50h b)24.00h c)12.20 d)22.40")
+    if antwort3 == "a" or antwort3 == "a)":
+        print("Sie haben es GESCHAFT ,fertig")
 
     else:
         print("Haha du Opfer")
 
         
-        else:
-            print("Haha du Opfer")
+    else:
+        print("Haha du Opfer")
 
 
-            else:
-                print("HAHA du Opfer")
+    else:
+        print("HAHA du Opfer")
                 
-   antwort = input("\nWollen sie es noch einmal versuchen? (yes/no) ")
+    antwort = input("\nWollen sie es noch einmal versuchen? (yes/no) ")
 
-   if antwort != "yes":
-      break
-   else:
-      print("\n\n")
-
+    if antwort != "yes":
+       break
+    else:
+       print("\n\n")
